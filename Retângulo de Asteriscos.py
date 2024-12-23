1845e6671c7e9620afcf12530487920157ac92fa
Luan Ramalho <32373552+LuanRamalho@users.noreply.github.com>
2024-12-23 16:06:20 -0300

    Add files via upload

---
+++ Retângulo de Asteriscos.py
@@ -0,0 +1,10 @@
+# Exibe um retângulo de asteriscos
+
+# Define as dimensões do retângulo
+altura = 10  # número de linhas
+largura = 30  # número de colunas
+
+# Loop para imprimir as linhas
+for _ in range(altura):
+    print('*' * largura)
+input()
